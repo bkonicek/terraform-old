317e1f000b7d15018368150dd2e5722e6c9ef4fc
Ben Konicek <ben.konicek@gmail.com>
2021-03-21 17:08:16 -0400

    create aws saml provider

--- global/iam/okta/main.tf
+++ global/iam/okta/main.tf
@@ -2,7 +2,7 @@ data "okta_app_metadata_saml" "saml_data" {
   app_id = "0oace7hozEaqkoHdj5d6"
 }
 
-# resource "aws_iam_saml_provider" "name" {
-#   name                   = "okta"
-#   saml_metadata_document = file("metadata.xml")
-# }
+resource "aws_iam_saml_provider" "name" {
+  name                   = "okta"
+  saml_metadata_document = data.okta_app_metadata_saml.saml_data.metadata
+}
