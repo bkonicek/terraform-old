707f0e1ef9287bb2ff69f85a6f8634889646952a
Ben Konicek <ben.konicek@gmail.com>
2021-03-21 17:47:11 -0400

    allow okta to list roles

--- global/iam/okta/main.tf
+++ global/iam/okta/main.tf
@@ -7,6 +7,30 @@ resource "aws_iam_saml_provider" "okta_saml" {
   saml_metadata_document = data.okta_app_metadata_saml.saml_data.metadata
 }
 
+resource "aws_iam_user" "okta" {
+  name = "OktaSsoUser"
+}
+
+resource "aws_iam_user_policy" "okta" {
+  name   = "OktaSsoPolicy"
+  user   = aws_iam_user.okta.name
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+          "Effect": "Allow",
+          "Action": [
+              "iam:ListRoles",
+              "iam:ListAccountAliases"
+          ],
+          "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
 module "okta_test_role" {
   source = "../../../modules/iam/okta_role"
 
@@ -16,23 +40,6 @@ module "okta_test_role" {
 
 }
 
-# resource "aws_iam_role" "okta_test" {
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRoleWithSAML"
-#         Effect = "Allow"
-#         Principal = {
-#           Federated = aws_iam_saml_provider.okta_saml.arn
-#         }
-#         Condition = {
-#           StringEquals = {
-#             "SAML:aud" = "https://signin.aws.amazon.com/saml"
-#           }
-#         }
-#       },
-#     ]
-#   })
-#   name = "okta-test"
+# resource "aws_iam_policy_attachment" "okta-test-policies" {
+
 # }
